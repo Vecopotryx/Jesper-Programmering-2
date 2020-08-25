5883290ecbb3939a6c92010acf8b9541a9617b2d
Vecopotryx <Repsejk@gmail.com>
2020-08-25 12:59:18 +0200

    Started working on first assignment

---
+++ Stuff/v35/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Play Windows XP shutdown sound.
+            Console.Beep(1661, 250); 
+            Console.Beep(1244, 250);
+            Console.Beep(830, 250);
+            Console.Beep(932, 250);
+
+            Console.WriteLine("Is your name Niklas?");
+            if(Console.ReadLine().ToLower().Equals("yes"))
+            {
+                Console.WriteLine("Hello Niklas");
+            }
+
+        }
+    }
+}
