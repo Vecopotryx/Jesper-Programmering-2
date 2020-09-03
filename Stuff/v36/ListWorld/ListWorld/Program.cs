8241406dc1f45e3fe4fe9259a043fd1bc5c94d37
Vecopotryx <repsejk@gmail.com>
2020-09-03 11:07:45 +0200

    Started working on new assignement

---
+++ Stuff/v36/ListWorld/ListWorld/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListWorld
+{
+    class Program
+    {
+
+        // Declare variables
+        static int userAmount;
+
+        static List<User> users = new List<User>();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many users would you like to add?");
+
+            userAmount = int.Parse(Console.ReadLine());
+
+            if (userAmount == 0)
+            {
+                Console.WriteLine("Nope");
+
+            }
+
+            AddUser();
+
+            foreach (var user in users)
+            {
+                Console.WriteLine("User: {0},{1},{2}", user.Name, user.Age, user.Alive);
+            }
+
+        }
+
+        static void AddUser()
+        {
+            Console.WriteLine("Adding user number " + (users.Count + 1));
+            users.Add(new User(GetName(), GetAge(), GetAlive()));
+
+        }
+
+
+        static string GetName()
+        {
+            Console.WriteLine("Please enter name:");
+            string nameIn = Console.ReadLine();
+            if (string.IsNullOrEmpty(nameIn))
+            {
+                GetName();
+            }
+            return nameIn;
+        }
+
+        static short GetAge()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Please enter age:");
+
+                    string consoleIn = Console.ReadLine();
+
+                    return short.Parse(consoleIn);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("That doesn't seem like a valid age. Please enter a valid short");
+                }
+            }
+        }
+
+        static bool GetAlive()
+        {
+            Console.WriteLine("Are you alive? (yes/no)");
+            if (Console.ReadLine().ToLower().Equals("yes"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
+
---
+++ Stuff/v36/ListWorld/ListWorld/User.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListWorld
+{
+    public class User
+    {
+        private string name;
+        private short age;
+        private bool alive;
+
+        public User()
+        {
+
+        }
+        public User(string name, short age, bool alive)
+        {
+            this.name = name;
+            this.age = age;
+            this.alive = alive;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public short Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public bool Alive
+        {
+            get { return alive; }
+            set { alive = value; }
+        }
+    }
+
+}
