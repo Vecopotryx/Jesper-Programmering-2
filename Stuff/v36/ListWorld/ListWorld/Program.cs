2a4d81f5c1dbdc9c32930c9de1e4e7aebb4944a1
Vecopotryx <repsejk@gmail.com>
2020-09-03 11:42:11 +0200

    Added the requested functionality

--- Stuff/v36/ListWorld/ListWorld/Program.cs
+++ Stuff/v36/ListWorld/ListWorld/Program.cs
@@ -7,38 +7,77 @@ namespace ListWorld
     {
 
         // Declare variables
-        static int userAmount;
-
         static List<User> users = new List<User>();
 
         static void Main(string[] args)
         {
+            CollectUserInfo(); // Collects name, age and more for up to 255 users.
+            PrintUserInfo(); // Prints the information of the users.
+            PrintReverseOrder(); // Reverses the names of the users and then prints it.
+            PromptExit(); // Waits for user input before killing process.
+        }
+
+        static void PromptExit()
+        {
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
+        }
+
+        static void CollectUserInfo()
+        {
             Console.WriteLine("How many users would you like to add?");
-
-            userAmount = int.Parse(Console.ReadLine());
-
-            if (userAmount == 0)
+            while (true)
             {
-                Console.WriteLine("Nope");
-
+                try
+                {
+                    byte userAmount = byte.Parse(Console.ReadLine());
+                    for (byte i = 0; i < userAmount; i++)
+                    {
+                        AddUser();
+                    }
+                    break;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("That doesn't appear to be a valid amount. Please enter a maximum value of 255");
+                }
             }
+            Console.Clear();
+        }
 
-            AddUser();
-
+        static void PrintUserInfo()
+        {
             foreach (var user in users)
             {
-                Console.WriteLine("User: {0},{1},{2}", user.Name, user.Age, user.Alive);
+                string lifeState;
+                if (user.Alive)
+                {
+                    lifeState = "alive";
+                }
+                else
+                {
+                    lifeState = "dead";
+                }
+                Console.WriteLine("{0} is {1} years old and is currently " + lifeState, user.Name, user.Age, user.Alive);
             }
-
+        }
+       static void PrintReverseOrder()
+        {
+            Console.WriteLine("The names in reverse order: ");
+            users.Reverse();
+            foreach (var user in users)
+            {
+                Console.Write(user.Name + " ");
+            }
+            Console.WriteLine(); // Move to next line
         }
 
         static void AddUser()
         {
+            Console.WriteLine(); // Adds empty line just to make it easier to read
             Console.WriteLine("Adding user number " + (users.Count + 1));
             users.Add(new User(GetName(), GetAge(), GetAlive()));
-
         }
-
 
         static string GetName()
         {
@@ -72,7 +111,7 @@ namespace ListWorld
 
         static bool GetAlive()
         {
-            Console.WriteLine("Are you alive? (yes/no)");
+            Console.WriteLine("Is the person alive? (yes/no)");
             if (Console.ReadLine().ToLower().Equals("yes"))
             {
                 return true;
