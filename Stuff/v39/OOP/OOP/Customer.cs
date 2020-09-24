b1c024efc393ab2b0c1ab5e6c6b3b776c64ee4f5
Vecopotryx <Repsejk@gmail.com>
2020-09-24 10:39:40 +0200

    Added a way to handle customer info

---
+++ Stuff/v39/OOP/OOP/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    class Customer
+    {
+        public string _name { get; set; }
+        public int _age { get; set; }
+        public string _paymentInfo { get; set; }
+
+    }
+}
--- Stuff/v39/OOP/OOP/Program.cs
+++ Stuff/v39/OOP/OOP/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace OOP
 {
     class Program
     {
+        static List<Customer> customers = new List<Customer>();
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        static void AddCustomer()
+        {
+            Console.WriteLine("Name: ");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Age: ");
+            var age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Payement info: ");
+            var payementInfo = Console.ReadLine();
+
+            customers.Add(new Customer{ _name = name, _age = age, _paymentInfo = payementInfo });
+
         }
     }
 }
